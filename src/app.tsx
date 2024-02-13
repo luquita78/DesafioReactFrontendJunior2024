428539bd9a438ba0463a41a06e858e8cd25b920e
luquita78 <lucasbarbosa.oliveira78@gmail.com>
2024-02-13 15:03:20 -0300

    Recuperando Tarefas via GET

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,36 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+
+interface Tarefa {
+  id: number;
+  title: string;
+  // Adicione outras propriedades se existirem
+}
 
 export default function App() {
+  const [tarefas, setTarefas] = useState<Tarefa[]>([]);
+
+  useEffect(()=>{
+    const fetchData = async () => {
+      try {
+        const res = await fetch('https://my-json-server.typicode.com/EnkiGroup/DesafioReactFrontendJunior2024/todos');
+        const data = await res.json();
+        setTarefas(data);
+      } catch (error) {
+        console.error("Erro ao obter tarefas:",error);
+      }
+    };
+    fetchData();
+  },[]);
   return (
     <section>
       <h1>Todos</h1>
+      <div>
+      <ul>
+        {tarefas.map(tarefa => (
+          <li key={tarefa.id}>{tarefa.title}</li>
+        ))}
+      </ul>
+    </div> 
     </section>
   );
 }
